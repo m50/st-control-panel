7e8cfc1b47ecded2e6cd2798ea1e5f3e420a546c
Marisa Clardy <marisa@clardy.eu>
2020-03-24 00:10:16 -0500

    Fix layout issues.

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,36 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch, Redirect } from "react-router-dom";
 import Sidebar from './structure/Sidebar'
 import UserIcon from './structure/UserIcon';
 
-function App() {
+export default () => {
   return (
     <Router>
-      <header className="bg-orange-400 flex items-center justify-between text-white">
+      <header className="w-screen fixed left-0 top-0 z-50 bg-orange-400 flex items-center justify-between text-white">
         <h1 className="text-xl my-2 mx-5">SpamTitan Control Panel</h1>
         <UserIcon />
       </header>
-      <div id="body" className="h-screen w-screen">
-        <Sidebar className="w-1/6 h-full bg-gray-800 inline-block" />
-        <div className="w-5/6 h-full inline-block">
-          <Switch>
-            <Route path="/">
-              Default Route
-            </Route>
-            <Route path="/dashboard">
-              Dashboard Route
-            </Route>
-          </Switch>
-        </div>
-      </div>
+      <Switch>
+        <Route path="/dashboard">
+          <BodyWrapper>
+            Dashboard Route
+          </BodyWrapper>
+        </Route>
+        <Route path="/">
+          <Redirect to="/dashboard" />
+        </Route>
+      </Switch>
     </Router>
   );
 }
 
-export default App;
+const BodyWrapper: React.FunctionComponent<{ children: string | JSX.Element | JSX.IntrinsicElements }> = (props) => {
+  return (
+    <div id="body" className="z-0 fixed top-0 left-0 h-screen w-screen flex">
+      <Sidebar className="pt-20 w-1/6 h-full bg-gray-700 inline-block" />
+      <div className="pt-20 w-5/6 h-full inline-block">
+        {props.children}
+      </div>
+    </div>
+  );
+}
